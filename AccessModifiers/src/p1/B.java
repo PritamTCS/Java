e11928eb4b0bf7d89063e7857c18d4ee57b0e848
Pritam <pritam123@gmail.com>
2019-04-30 08:31:48 +0530

    Updated

---
+++ AccessModifiers/src/p1/B.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class B extends A  {
+
+	public static void main(String[] args) {
+		A objA = new A();
+		B objB = new B();
+
+		System.out.println(objB.b);
+		System.out.println(objB.c);
+		System.out.println(objB.d);
+	}
+
+}
